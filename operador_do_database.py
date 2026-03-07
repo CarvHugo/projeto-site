421c9538a424d7ab4facc0b88684f32a02b44fab
Hugo Carvalho <hugoncarv@gmail.com>
2026-03-07 20:04:39 -0300

    feat: adição parcial de edição de dados
    - adiciona parcialmente a opção 4 (edição de registros) na interface, realocando "sair do sistema" para a opção 5

--- operador_do_database.py
+++ operador_do_database.py
@@ -16,6 +16,7 @@ def listagem():
         opcoes = ['Ver alimentos cadastrados.',
                   'Cadastrar novo alimento.',
                   'Deletar registro.',
+                  'Editar registro.',
                   'Sair do sistema.']
         
         def retornar(voltar=''):
@@ -36,9 +37,9 @@ def listagem():
 
         try:
             escolha = int(input('\033[32mSua Opção: \033[m'))
-            if 1 > escolha or escolha > 4:
+            if 1 > escolha or escolha > 5:
                 os.system('cls')
-                print('\033[31mEscolha um valor inteiro válido de "1" a "4".\033[m')
+                print('\033[31mEscolha um valor inteiro válido de "1" a "5".\033[m')
         
         except ValueError:
             os.system('cls')
@@ -199,6 +200,7 @@ def listagem():
                     cursor.execute(f"""DELETE FROM produtos WHERE id = {numero_id};""")
                     conexao.commit()
                     print('\n\033[32mProduto deletado!\033[m')
+                    retornar()
                 
                 elif numero_id.isalpha() and numero_id in ('V','v'):
                     os.system('cls')
@@ -211,9 +213,46 @@ def listagem():
                         sleep(0.03)
                         print('\033[31mEntrada inválida! Digite apenas o número ID ou v.\033[m')
                         numero_id = str(input('ID: '))
+                        
+                        if numero_id in ('V', 'v'):
+                            os.system('cls')
+                            retornar(numero_id)
+                        
+                        else:
+                            if numero_id.isnumeric():
+                                numero_id = int(numero_id)
+                                cursor.execute(f"""DELETE FROM produtos WHERE id = {numero_id};""")
+                                conexao.commit()
+                                print('\n\033[32mProduto deletado!\033[m')
+                                retornar()
+                                numero_id = str(numero_id)
 
-            
             elif escolha == 4:
+                os.system('cls')
+                print('-' * 58)
+                sleep(0.03)
+                print('\033[33mEDIÇÃO DE REGISTROS\033[m'.center(65))
+                sleep(0.03)
+                print('-' * 58)
+                sleep(0.03)
+
+                opcoes_edicao = [
+                    'Editar preço.',
+                    'Editar categoria.',
+                    'Editar nome.'
+                          ]
+                for numero, opcao in enumerate(opcoes_edicao):
+                    print(f'\033[33m{numero + 1}\033[m - \033[34m{opcao}\033[m')
+                    sleep(0.03)
+                print('\nou')
+                sleep(0.03)
+                print('\n\033[34mDigite v para voltar.\033[m')
+                sleep(0.03)
+                print('-' * 58)
+                sleep(0.03)
+                escolha_2 = int(input('\033[32mSua opção: \033[m'))
+            
+            elif escolha == 5:
                 os.system('cls')
                 print('-' * 58)
                 sleep(0.03)
