0837f3664098b7bbdf68bda3f699ab8dda533d6e
Hugo Carvalho <hugoncarv@gmail.com>
2026-03-11 10:29:40 -0300

    refactor: readiciona a lista de alimentos e melhorias gerais
    - ao digitar 1 no menu principal, é mostrada a listagem conforme consta no banco de dados
    
    - as mensagens de erro ficam mais visíveis
    
    - estrutura_de_menu() ganha nova condição para organizar o texto, dependendo do título
    
    - retornar() ganha nova condição de parâmetro para automatizar a animação de retorno entre as telas

--- operador_refatorado.py
+++ operador_refatorado.py
@@ -10,6 +10,15 @@ def estrutura_de_menu(titulo):
     sleep(0.03)
     print(titulo.center(65))
     sleep(0.03)
+    
+    if titulo not in ('\033[33mMENU PRINCIPAL\033[m', '\033[33mNOVO CADASTRO\033[m', '\033[33mALIMENTOS CADASTRADOS\033[m'):
+        print('-' * 58)
+        sleep(0.03)
+        print('ou')
+        sleep(0.03)
+        print('\033[34mPressione ENTER para voltar\033[m')
+        sleep(0.03)
+        
     print('-' * 58)
     sleep(0.03)
 
@@ -29,10 +38,14 @@ def mostrar_opcoes(opcoes):
     
     
 def retornar(voltar=''):
-    while voltar == 'V':
+    while voltar == 'manualmente':
         voltar = str(input('\033[34mPressione ENTER para voltar \033[m'))
         os.system('cls')
     
+    while voltar not in ('manualmente', ''):
+        voltar = str(input('\033[31mDigite apenas ENTER para voltar\033[m'))
+        os.system('cls')
+        
     for ciclo in range(2):
                 for pontos in range(4):
                     print('\rVoltando' + '.' * pontos, end='', flush=True)
@@ -40,29 +53,26 @@ def retornar(voltar=''):
                 os.system('cls')
 
 def listagem():
+    erro = ''
     while True:
         estrutura_de_menu('\033[33mMENU PRINCIPAL\033[m')
         mostrar_opcoes(opcoes_menu_principal)
+        
+        if erro:
+            print(erro)
+        
+        erro = ''
 
         try:
             escolha = int(input('\033[32mSua Opção: \033[m'))
             
-            while 1 > escolha or escolha > 5:
+            if 1 > escolha or escolha > 5:
                 os.system('cls')
-                estrutura_de_menu('\033[33mMENU PRINCIPAL\033[m')
-                mostrar_opcoes(opcoes_menu_principal)
-                print('\033[31mEscolha um valor inteiro válido de "1" a "5".\033[m')
-                
-                try:
-                    escolha = int(input('\033[32mSua Opção: \033[m'))
-                    
-                except ValueError:
-                    os.system('cls')
-                    print('\033[31mDigite um número inteiro!\033[m')
-    
+                erro = '\033[31mEscolha um número inteiro válido de "1" a "5".\033[m'
+        
         except ValueError:
             os.system('cls')
-            print('\033[31mDigite um número inteiro!\033[m')
+            erro = '\033[31mDigite um número inteiro!\033[m'
         
         except KeyboardInterrupt:
             os.system('cls')
@@ -71,6 +81,15 @@ def listagem():
         
         else:
             if escolha == 1:
+                os.system('cls')
                 estrutura_de_menu('\033[33mALIMENTOS CADASTRADOS\033[m')
-    
+                cursor.execute("""SELECT * FROM produtos;""")
+                rows = cursor.fetchall()
+                print(f' {"ID":<5} {"Nome":<26} {"Categoria":<15} {"Preço":<10}')
+                for id, nome, categoria, preco in rows:
+                    print(f' {id:<5} {nome:<26} {categoria:<15}R$ {preco:<10.2f}')
+                    sleep(0.03)
+                print('-' * 58)
+                retornar('manualmente')
+
 listagem()
